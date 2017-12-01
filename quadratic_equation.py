17b846cf9a0f556ab8e232f72d59b88cc01a1363
BaisakKG <baisak40@gmail.com>
2017-12-01 11:26:44 +0600

    Add value quadrat

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,9 +2,10 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(abs(discriminant))) / (2 * a)
-    root2 = (-b + sqrt(abs(discriminant))) / (2 * a)
+    quadrat = 2
+    discriminant = b ** quadrat - 4 * a * c
+    root1 = (-b - sqrt(abs(discriminant))) / (quadrat * a)
+    root2 = (-b + sqrt(abs(discriminant))) / (quadrat * a)
     if discriminant == 0:
         return root1, None
     elif discriminant < 0:
